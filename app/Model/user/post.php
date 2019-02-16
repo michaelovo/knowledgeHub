2bfa75b83258db813af1a49fdbbe1432f0fe92c7
michaelovo <emikeovo@gmail.com>
2019-02-15 21:12:15 -0800

    Added 'withTimestamps()' to the eloquent relationship

--- app/Model/user/post.php
+++ app/Model/user/post.php
@@ -9,10 +9,10 @@ class post extends Model
     // Relationship btw post and tag .
     // A single post can have many tags, and one tag can be included in many posts 'many->many'
     public function tags(){
-      return $this->belongsToMany('App\Model\user\tag','post_tags');// "post_tag" the table model name
+      return $this->belongsToMany('App\Model\user\tag','post_tags')->withTimestamps();// "post_tag" the table model name
     }
-
+    // '->withTimestamps()' allows "created_at" and "updated_at" time to be submitted to database
     public function category(){
-      return $this->belongsToMany('App\Model\user\category','category_posts');// "post_tag" the table model name
+      return $this->belongsToMany('App\Model\user\category','category_posts')->withTimestamps();// "post_tag" the table model name
     }
 }
