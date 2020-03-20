e024b64f47f75d3c41a2f3af5ef55ecd36e1b3a7
michael <emikeovo@gmail.com>
2020-03-20 16:36:30 +0100

    Fixed minor issue with project assets

--- app/Http/Controllers/Admin/PermissionController.php
+++ app/Http/Controllers/Admin/PermissionController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-use App\Model\admin\permission;
+use App\Model\admin\Permission;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
